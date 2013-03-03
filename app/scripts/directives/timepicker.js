2e2caf68159d2bb525fbe3cc3e1a7596a704b765
Sebastian Kusnier <sebastian@kusnier.net>
2013-03-03 19:53:26 +0100

    Add tootstrap-timepicker directive

---
+++ app/scripts/directives/timepicker.js
@@ -0,0 +1,36 @@
+angular.module('bTimepicker', []).
+  directive('bTimepicker', function($timeout){
+    return {
+      require: '?ngModel',
+      restrict: 'A',
+      link: function($scope, element, attrs, ngModelCtrl) {
+        var originalRender, updateModel;
+        updateModel = function(ev) {
+          return $scope.$apply(function() {
+            return ngModelCtrl.$setViewValue(ev.time.value);
+          });
+        };
+        if (ngModelCtrl != null) {
+          originalRender = ngModelCtrl.$render;
+          ngModelCtrl.$render = function() {
+            originalRender();
+            if (ngModelCtrl.$viewValue != undefined) {
+              $timeout( function() {element.timepicker('setTime', ngModelCtrl.$viewValue)}, 0 );
+            }
+            return ngModelCtrl.$viewValue;
+          };
+        }
+        return attrs.$observe('bTimepicker', function(value) {
+          var options;
+          options = {};
+          if (angular.isObject(value)) {
+            options = value;
+          }
+          if (typeof(value) === "string" && value.length > 0) {
+            options = angular.fromJson(value);
+          }
+          return element.timepicker(options).on('changeTime.timepicker', updateModel);
+        });
+      }
+    };
+  });
