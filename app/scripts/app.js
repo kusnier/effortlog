6dd263f96854e2d3c0fc3c23afd5d94fbb3fb8fb
Sebastian Kusnier <sebastian@kusnier.net>
2013-03-28 22:56:06 +0100

    Don't reload view on location.search changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,7 +5,8 @@ angular.module('effortlogApp', ['ui', 'bTimepicker', 'LocalStorageModule'])
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
+        reloadOnSearch: false
       })
       .otherwise({
         redirectTo: '/'
